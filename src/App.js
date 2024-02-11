1e87f269b81dbdbb2ec994372da9a1fd8e48f5f7
Sam <jainsambhav2021@gmail.com>
2024-02-11 16:10:07 +0530

    Ho ga

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './HomePage/Home.jsx'
+import Navbar from './Navbar/Navbar.jsx';
+import About from './About/about.jsx'
+function App() {
+  return (
+    <>
+    <Navbar/>
+      <Home />
+      <About/>
+    </>
+  );
+}
+
+export default App;
